c41b59468281be102538998d36619b42e6949db2
mladenmilic <56686882+mladenmilic@users.noreply.github.com>
2020-01-10 09:28:39 +0100

    Update app-routing.module.ts

--- Pizzeria/src/app/app-routing.module.ts
+++ Pizzeria/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewOrderComponent } from './components/new-order/new-order.component';
 import { ListOrdersComponent } from './components/list-orders/list-orders.component';
+import { ReviewOffersComponent } from './components/review-offers/review-offers.component';
 
 
 const routes: Routes = [
-  { path: 'review-offers', component: ListOrdersComponent},
+  { path: 'review-offers', component: ReviewOffersComponent},
   { path: 'list-orders', component: ListOrdersComponent},
   { path: 'new-order', component: NewOrderComponent},
   { path: 'login', component: LoginComponent },
